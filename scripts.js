0cb0729fd91e2dca0a8523db9d9f1da49df312b9
Alex Stevenson <alexstevenson19@gmail.com>
2017-01-05 23:55:59 -0600

    basic solution

--- scripts.js
+++ scripts.js
@@ -1,3 +1,27 @@
 //Use this file to implement Part One of your project
 
-console.log("test")
+// console.log("test")
+
+var animal = {}
+
+animal.username = "Daffy";
+
+// console.log(animal['username']);
+
+animal["tagline"] = "Ducks Rule!";
+
+var empty = [];
+animal.noises = empty;
+
+console.log(animal);
+
+var i = 0
+for(var key in animal){
+	i += 1;
+	console.log(i);
+	// console.log(animal[key]);
+	if (key == "username")
+		console.log(animal.username);
+	else if (key == "tagline")
+		console.log(animal.tagline);
+}
