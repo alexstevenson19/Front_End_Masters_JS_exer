3099a0e2d4e01a1a0f619ae7ca0d46d41e9dbf5b
Alex Stevenson <alexstevenson19@gmail.com>
2017-01-16 23:34:05 -0600

    slightly refactored animalMaker

--- constructor_loop.js
+++ constructor_loop.js
@@ -1,4 +1,4 @@
-function AnimalMaker(name) {
+var AnimalMaker = function(name) {
 	return {
 		speak: function(){
 			console.log("My name is, ", name);
@@ -12,8 +12,14 @@ var animalNames = ['Sheep', 'Liger', 'Big Bird'];
 
 var farm = [];
 
+// in this case we aren't using the keyword new, to keep it simplified in this case. It is just returning the object(literal).
 for(var i = 0; i < animalNames.length; i++) {
 	farm[i] = AnimalMaker(animalNames[i]);
+	// farm.push(AnimalMaker(animalNames[i]));
 }
 
 console.log(farm);
+
+for(var i = 0; i < farm.length; i++) {
+	farm[i].speak();
+}
