9e64e45e2d98a1f6d8a4f18994317723444c1727
Alex Stevenson <alexstevenson19@gmail.com>
2017-06-09 18:47:39 -0500

    comparing my solution to instructor solution, adjusted my addFriend function to match hers, commented out my original solution which called functions in functions that made things more complicated.

--- scripts.js
+++ scripts.js
@@ -69,37 +69,61 @@ var AnimalTestUser = function(username){
 	}
 }
 
-var addFriend = function(animalFriends){
-	var friends = []
-	if (animalFriends){
-		return friends = animalFriends.username
-	}
-}
+// my original solution for addFriend=========================================
+// I thought it only took one argument, the friends so it complicated things and I had to jump through extra hoops to avoid conflicts, but got it to work. Her solution, is to give the animal (that already exists, and then the friend)
+
+// var addFriend = function(animalFriends){
+// 	var friends = []
+// 	if (animalFriends){
+// 		return friends = animalFriends.username
+// 	}
+// }
 
 var AnimalCreator = function(info){
-	for(var i=0; i<arguments.length; i++){
+	// for(var i=0; i<arguments.length; i++){
 		username = arguments[0];
 		species = arguments[1];
 		tagline = arguments[2];
 		noises = arguments[3];
-		var friendName = addFriend(arguments[4]);
+		friends = [];
+		// original solution when I called the function I made above.======================
+		// var friendName = addFriend(arguments[4]);
 
-		if (addFriend == undefined) {
-			return { username, species, tagline, noises 
-			}
-		}
-		else {
-			return { username, species, tagline, noises, friendName
-			}
-		}
-	}
+		// if (addFriend == undefined) {
+		// 	return { username, species, tagline, noises 
+		// 	}
+		// }
+		// else {
+		// 	return { username, species, tagline, noises, friendName
+		// 	}
+		// }
+	// }
+	return {username, species, tagline, noises, friends}
 }
 
-var goose = AnimalCreator("George", "grey goose", "fly south", ['honk', 'hiss'], mallard)
-var mallard = AnimalCreator("Bob", "duck", 'You can count on me!', ['quack', 'arrgg', 'chewchewchew'], goose )
+function addFriend(animal, friend){
+		animal.friends.push(friend.username);
+}
+
+var myFarm = function(farmAnimals){
+	barn = []
+	for(var i=0; i<arguments.length; i++){
+		barn.push(farmAnimals[i]);
+	}
+		return(barn);
+}
+
+var goose = AnimalCreator('George', 'grey goose', 'fly south', ['honk', 'hiss']);
+//  original solution, I called the friend in the argument while creating the animal, instead of after when when the animal object already exists.
+// var mallard = AnimalCreator('Bob', 'duck', 'You can count on me!', ['quack', 'arrgg', 'chewchewchew'], goose );
+var mallard = AnimalCreator('Bob', 'duck', 'You can count on me!', ['quack', 'arrgg', 'chewchewchew']);
+addFriend(goose, mallard);
+var swan = AnimalCreator('Mary', 'white swan', 'I\'m not a pedal boat.', ['gargle', 'whoosh'] );
+var farmCollection = myFarm(goose, mallard, swan);
 
 console.log("Part 3- Functions ===================================")
-// console.log(AnimalTestUser("Bob", "duck", "cat"));
+console.log(AnimalTestUser("Bob", "duck", "cat"));
 console.log(mallard);
-
+console.log(goose);
+console.log(farmCollection);
 
